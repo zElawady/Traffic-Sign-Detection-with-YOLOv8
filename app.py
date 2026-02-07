c13f1bad7f9cb2dd00514e0563dbdb1e6d667328
zElawady <zeyadalawady@gmail.com>
2026-02-07 04:22:23 +0200

    Add files via upload
    the last version

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+import streamlit as st
+from ultralytics import YOLO
+from PIL import Image
+import cv2
+import numpy as np
+
+@st.cache_resource
+def load_model():
+    return YOLO("traffic_signs_model.pt")
+
+model = load_model()
+
+st.title("Traffic Sign Detection with YOLOv8")
+
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+
+    img_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    results = model(img_cv)
+
+    annotated_img = results[0].plot()
+    annotated_pil = Image.fromarray(cv2.cvtColor(annotated_img, cv2.COLOR_BGR2RGB))
+
+    st.image(annotated_pil, caption="Detected Traffic Signs", use_container_width=True)
+
+    if results[0].boxes:
+        for box in results[0].boxes:
+            cls = int(box.cls)
+            conf = float(box.conf)
+            st.write(f"{model.names[cls]} — {conf:.2f}")
